fdb99ebcee818bf90aae9cfbc7fe617b24365993
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-11-24 11:25:04 +0100

    Add project files.

---
+++ Uppgift 2 Tre i rad/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Skriv hur mycket N är värt");
+            int N = int.Parse(Console.ReadLine());
+            int j = 0;
+            
+            for (int i = 1; i * (i + 1) * (i + 2) < N; i++)
+            {
+                int E = i * (i+1) * (i+2);
+                Console.WriteLine(E);
+
+                j++;
+            }
+            Console.WriteLine("Det finns " + j + " stycken treiga tal under " + N);
+        }
+    }
+}
